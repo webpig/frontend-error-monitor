acee8551695f2e6f6c2e3cf77c6a20dbbe0c53e9
webpig <1176461@qq.com>
2021-08-22 22:55:41 +0800

    feat: 原生js错误监听与测试

--- src/vanilla/index.ts
+++ src/vanilla/index.ts
@@ -4,7 +4,25 @@ function handleError(
   lineno?: number,
   colno?: number,
   error?: Error) {
-  console.log('message: ', message, 'source: ', source, 'lineno: ', lineno, 'colno: ', colno, 'error', error)
+  console.table({
+    message,
+    source,
+    lineno,
+    colno,
+    error
+  })
 }
 
+// 监听js运行错误
 window.onerror = handleError
+
+// 监听js运行错误，资源加载错误
+window.addEventListener('error', (e) => {
+  console.log('e', e)
+})
+
+// 监听异步错误
+window.addEventListener('unhandledrejection', (e) => {
+  e.preventDefault()
+  console.log('unhandledrejection', e.reason)
+})
